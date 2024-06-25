1bb578649d0f60eabbded3ee90ebbd03f098d7e3
Sazon Mahmud <developer.sazon@gmail.com>
2024-06-25 18:26:45 +0600

    View each product when any user click the view button and also get the product details

--- src/viewProduct.js
+++ src/viewProduct.js
@@ -8,30 +8,41 @@ import {
   MDBModalTitle,
   MDBModalBody,
   MDBModalFooter,
+  MDBRow,
+  MDBCol,
+  MDBCardImage,
 } from 'mdb-react-ui-kit';
-import ViewData from './productData';
 
-export default function ViewProduct() {
+
+export default function ViewProduct(data) {
   const [centredModal, setCentredModal] = useState(false);
-
   const toggleOpen = () => setCentredModal(!centredModal);
-
+ 
   return (
-    <>
-      <MDBBtn onClick={toggleOpen}>View</MDBBtn>
-
+    <>  
+      
+      <MDBBtn onClick={toggleOpen}>View</MDBBtn> 
       <MDBModal tabIndex='-1' open={centredModal} onClose={() => setCentredModal(false)}>
         <MDBModalDialog centered size='xl'>
           <MDBModalContent>
             <MDBModalHeader>
-              <MDBModalTitle>Modal title</MDBModalTitle>
+              <MDBModalTitle>{data.product.name}</MDBModalTitle>
               <MDBBtn className='btn-close' color='none' onClick={toggleOpen}></MDBBtn>
             </MDBModalHeader>
             <MDBModalBody>
-              <p>
-                Cras mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis in,
-                egestas eget quam. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
-              </p>
+                 <MDBRow>
+                     <div className="d-flex">
+                          <div className="p-2 flex-shrink-1">
+                               <MDBCardImage className='img-thumbnail' src={data.product.images[0]?.src} position='top' alt='...'/>
+                          </div>
+                          <div className="p-2 w-75">
+                               <p className='h4 text-start'>{data.product.title}</p>
+                               <p className='h3 text-start justify-content'>Price: {data.product.price} tk</p>
+                               <p className='text-start justify-content'>{data.product.description}</p>
+                               <a href={data.product.permalink} target="_blank" rel="noopener noreferrer"><MDBBtn className='bg-primary'>Buy Now</MDBBtn></a>
+                          </div>
+                    </div>
+                 </MDBRow>
             </MDBModalBody>
             <MDBModalFooter>
               <MDBBtn color='secondary' onClick={toggleOpen}>
@@ -39,6 +50,7 @@ export default function ViewProduct() {
               </MDBBtn>
             </MDBModalFooter>
           </MDBModalContent>
+          
         </MDBModalDialog>
       </MDBModal>
     </>
