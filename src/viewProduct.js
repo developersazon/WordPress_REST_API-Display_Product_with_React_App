8063448f8d8390e0c78bcad4e6df06a90f874a09
Sazon Mahmud <developer.sazon@gmail.com>
2024-06-26 12:25:21 +0600

    In this view product added some information like as product buy link, product code, title etc

--- src/viewProduct.js
+++ src/viewProduct.js
@@ -17,6 +17,7 @@ import {
 export default function ViewProduct(data) {
   const [centredModal, setCentredModal] = useState(false);
   const toggleOpen = () => setCentredModal(!centredModal);
+  console.log(data);
  
   return (
     <>  
@@ -32,13 +33,13 @@ export default function ViewProduct(data) {
             <MDBModalBody>
                  <MDBRow>
                      <div className="d-flex">
-                          <div className="p-2 flex-shrink-1">
-                               <MDBCardImage className='img-thumbnail' src={data.product.images[0]?.src} position='top' alt='...'/>
+                          <div className="p-2">
+                               <MDBCardImage src={data.product.images[0]?.src} position='top' alt='...' width={350} height={350}/>
                           </div>
                           <div className="p-2 w-75">
-                               <p className='h4 text-start'>{data.product.title}</p>
-                               <p className='h3 text-start justify-content'>Price: {data.product.price} tk</p>
-                               <p className='text-start justify-content'>{data.product.description}</p>
+                               <p className='h5 text-start'>Product Code: {data.product.id}</p>
+                               <p className='h5 text-start'>Price: {data.product.price} tk</p>
+                               <p className='' style={{textAlign:'justify'}}>{data.product.description}</p>
                                <a href={data.product.permalink} target="_blank" rel="noopener noreferrer"><MDBBtn className='bg-primary'>Buy Now</MDBBtn></a>
                           </div>
                     </div>
